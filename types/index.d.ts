5f35edd250077e831bddc8d3723ffa29fa7b7f04
Mattia-Longhin <mattia@appunto.io>
2021-11-12 16:53:47 +0100

    Added Mailer types

--- types/index.d.ts
+++ types/index.d.ts
@@ -14,3 +14,24 @@ export declare function createRootAccount(
   password      : string,
   options?      : {rootRole? : string;}
 ): null | string;
+
+export declare class Mailer {
+  constructor(options : any);
+  async send(templateId : string | number, to : string, params ?: Record<string, string>) : Promise<true | undefined>;
+  async sendLostPassword(to : string, options ?: SibSendOptions) : Promise<true | undefined>;
+  async sendWelcome(to : string, options ?: SibSendOptions) : Promise<true | undefined>;
+  async sendPasswordlessCode(to : string, options ?: SibSendOptions) : Promise<true | undefined>;
+}
+
+export declare class SibMailer extends Mailer {
+  constructor(options : SibMailerOptions)
+}
+export declare class TestMailer extends Mailer {}
+
+export declare type SibMailerOptions = {
+  apiKey : string;
+  mapTemplateId : (type : string, options ?: SibSendOptions) => number;
+}
+export declare type SibSendOptions = {
+  data ?: Record<string, string>;
+}
